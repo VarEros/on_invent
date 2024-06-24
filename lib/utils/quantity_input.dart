184b75ac66bde46c675f94d5dba0d5e4910b70d7
ErOs <104714204+VarEros@users.noreply.github.com>
2024-06-23 18:35:24 -0600

    quantity input diy

---
+++ lib/utils/quantity_input.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class QuantityInput extends StatefulWidget {
+  final ValueChanged<int> quantity;
+  final String label;
+  const QuantityInput({ super.key, required this.quantity, required this.label});
+
+  @override
+  State<QuantityInput> createState() => _QuantityInputState();
+}
+
+class _QuantityInputState extends State<QuantityInput> {
+  int quantity = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(widget.label, style: const TextStyle(fontWeight: FontWeight.bold)),
+        Row(
+          children: [
+            IconButton(onPressed: () => setState(()=> widget.quantity(--quantity)) , icon: const Icon(Icons.remove)),
+            Text(quantity.toString(), style: const TextStyle(fontSize: 20)),
+            IconButton(onPressed: () => setState(() => widget.quantity(++quantity)), icon: const Icon(Icons.add)),
+          ],
+        ),
+      ],
+    );
+  }
+}
--- lib/widgets/inventary_dialog.dart
+++ lib/widgets/inventary_dialog.dart
@@ -1,34 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:quantity_input/quantity_input.dart';
+import 'package:on_invent/utils/quantity_input.dart';
 
 class InventaryDialog extends StatelessWidget {
   const InventaryDialog({ super.key });
 
   @override
-  Widget build(BuildContext context){
-    int xsCant = 0;
-    int sCant = 0;
-    int mCant = 0;
-    int lCant = 0;
-    int xlCant = 0;
+  Widget build(BuildContext context) {
+    List<int> inventary = [0, 0, 0, 0, 0, 0];
 
     return AlertDialog(
+    
       title: const Text('Añadir Inventario'),
-      content: Column(
+      content: 
+      // grid of inventary
+      Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          QuantityInput(
-            label: 'XS',
-            acceptsZero: true,
-            value: 14,
-            onChanged: (value) {
-              xsCant = int.parse(value.replaceAll(',', ''));
-            },
-            minValue: 0,
-            maxValue: 100,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              QuantityInput(quantity: (value) => inventary[0] = value, label: 'XS'),
+              QuantityInput(quantity: (value) => inventary[1] = value, label: 'S'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              QuantityInput(quantity: (value) => inventary[2] = value, label: 'M'),
+              QuantityInput(quantity: (value) => inventary[3] = value, label: 'L'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              QuantityInput(quantity: (value) => inventary[4] = value, label: 'XL'),
+              QuantityInput(quantity: (value) => inventary[5] = value, label: 'XXL'),
+            ],
           ),
         ],
       ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: const Text('Cancelar'),
+        ),
+        TextButton(
+          onPressed: () {
+          },
+          child: const Text('Añadir'),
+        ),
+      ],
     );
   }
+  
+
 }
